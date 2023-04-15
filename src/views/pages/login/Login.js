00e44d94acdb1ede4c1b268dbc7d23c3271912a1
()gres <serdarsaryjayev2002@gmail.com>
2023-04-15 17:27:27 +0500

    Authorization

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import {
   CButton,
   CCard,
@@ -12,11 +12,54 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
+  CAlert
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
+import axios from 'axios'
+import setting from '../../../setting.json'
+import Fetch from 'src/fetch'
+
 
 const Login = () => {
+  const [alert, setAlert] = useState({ title: '', status: false })
+
+  const navigate = useNavigate();
+
+  const LoginFunc = async (e) => {
+    e.preventDefault()
+    let body = {}
+    for (let i in e.target.elements) {
+      body[e.target.elements[i].name] = e.target.elements[i].value
+    }
+    let result = await axios.post(`${setting.IP}/user/login`, body)
+      .catch((err) => {
+        setAlert({
+          title: err.response.data.msg,
+          status: true
+        })
+      })
+
+
+    if (!!result) {
+      localStorage.setItem("img", result.data.image);
+      localStorage.setItem("nick", result.data.nick);
+      localStorage.setItem('token', result.data.token);
+      localStorage.setItem('type', result.data.type)
+    }
+    if (localStorage.type > 2) {
+      navigate('/')
+      document.location.reload()
+    } else {
+      setAlert({
+        title: 'You are not admin',
+        status: true
+      })
+    }
+
+
+
+  }
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -25,20 +68,24 @@ const Login = () => {
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
-                  <CForm>
+                  <CForm onSubmit={(e) =>
+                    LoginFunc(e)
+                  }>
                     <h1>Login</h1>
                     <p className="text-medium-emphasis">Sign In to your account</p>
                     <CInputGroup className="mb-3">
                       <CInputGroupText>
                         <CIcon icon={cilUser} />
                       </CInputGroupText>
-                      <CFormInput placeholder="Username" autoComplete="username" />
+                      <CFormInput placeholder="Email" name='email' autoComplete="email" />
+
                     </CInputGroup>
                     <CInputGroup className="mb-4">
                       <CInputGroupText>
                         <CIcon icon={cilLockLocked} />
                       </CInputGroupText>
                       <CFormInput
+                        name='password'
                         type="password"
                         placeholder="Password"
                         autoComplete="current-password"
@@ -46,20 +93,22 @@ const Login = () => {
                     </CInputGroup>
                     <CRow>
                       <CCol xs={6}>
-                        <CButton color="primary" className="px-4">
+                        <CButton type='submit' color="primary" className="px-4">
                           Login
                         </CButton>
                       </CCol>
-                      <CCol xs={6} className="text-right">
+                      {/* <CCol xs={6} className="text-right">
                         <CButton color="link" className="px-0">
                           Forgot password?
                         </CButton>
-                      </CCol>
+                      </CCol> */}
                     </CRow>
                   </CForm>
                 </CCardBody>
+                <CAlert visible={alert.status} color="danger">{alert.title}</CAlert>
               </CCard>
-              <CCard className="text-white bg-primary py-5" style={{ width: '44%' }}>
+
+              {/* <CCard className="text-white bg-primary p-4" >
                 <CCardBody className="text-center">
                   <div>
                     <h2>Sign up</h2>
@@ -74,7 +123,7 @@ const Login = () => {
                     </Link>
                   </div>
                 </CCardBody>
-              </CCard>
+              </CCard> */}
             </CCardGroup>
           </CCol>
         </CRow>
--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -10,7 +10,9 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
+  CCardGroup
 } from '@coreui/react'
+import { Link } from 'react-router-dom'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 
@@ -19,52 +21,71 @@ const Register = () => {
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
         <CRow className="justify-content-center">
-          <CCol md={9} lg={7} xl={6}>
-            <CCard className="mx-4">
-              <CCardBody className="p-4">
-                <CForm>
-                  <h1>Register</h1>
-                  <p className="text-medium-emphasis">Create your account</p>
-                  <CInputGroup className="mb-3">
-                    <CInputGroupText>
-                      <CIcon icon={cilUser} />
-                    </CInputGroupText>
-                    <CFormInput placeholder="Username" autoComplete="username" />
-                  </CInputGroup>
-                  <CInputGroup className="mb-3">
-                    <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" />
-                  </CInputGroup>
-                  <CInputGroup className="mb-3">
-                    <CInputGroupText>
-                      <CIcon icon={cilLockLocked} />
-                    </CInputGroupText>
-                    <CFormInput
-                      type="password"
-                      placeholder="Password"
-                      autoComplete="new-password"
-                    />
-                  </CInputGroup>
-                  <CInputGroup className="mb-4">
-                    <CInputGroupText>
-                      <CIcon icon={cilLockLocked} />
-                    </CInputGroupText>
-                    <CFormInput
-                      type="password"
-                      placeholder="Repeat password"
-                      autoComplete="new-password"
-                    />
-                  </CInputGroup>
-                  <div className="d-grid">
-                    <CButton color="success">Create Account</CButton>
+          <CCol md={8}>
+            <CCardGroup>
+              <CCard  className="text-white text-center bg-primary p-4" >
+                <CCardBody>
+                  <div>
+                    <h2>Login</h2>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                    <Link to="/login">
+                      <CButton color="primary" className="mt-3" active tabIndex={-1}>
+                        Login
+                      </CButton>
+                    </Link>
                   </div>
-                </CForm>
-              </CCardBody>
-            </CCard>
+                </CCardBody>
+              </CCard>
+              <CCard className="p-4"  >
+                <CCardBody className="text-center">
+                  <CForm>
+                    <h1>Register</h1>
+                    <p className="text-medium-emphasis">Create your account</p>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilUser} />
+                      </CInputGroupText>
+                      <CFormInput placeholder="Username" autoComplete="username" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>@</CInputGroupText>
+                      <CFormInput placeholder="Email" autoComplete="email" />
+                    </CInputGroup>
+                    <CInputGroup className="mb-3">
+                      <CInputGroupText>
+                        <CIcon icon={cilLockLocked} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Password"
+                        autoComplete="new-password"
+                      />
+                    </CInputGroup>
+                    <CInputGroup className="mb-4">
+                      <CInputGroupText>
+                        <CIcon icon={cilLockLocked} />
+                      </CInputGroupText>
+                      <CFormInput
+                        type="password"
+                        placeholder="Repeat password"
+                        autoComplete="new-password"
+                      />
+                    </CInputGroup>
+                    <div className="d-grid">
+                      <CButton color="success">Create Account</CButton>
+                    </div>
+                  </CForm>
+                </CCardBody>
+              </CCard>
+            </CCardGroup>
           </CCol>
         </CRow>
       </CContainer>
     </div>
+
   )
 }
 

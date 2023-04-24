a4e9fa5af6448c28f0b3276bd3597ad3446017cb
()gres <serdarsaryjayev2002@gmail.com>
2023-04-24 16:14:15 +0500

    Other setting

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -63,8 +63,8 @@ const Login = () => {
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
-        <CRow className="justify-content-center">
-          <CCol md={8}>
+        <CRow  className="justify-content-center">
+          <CCol  md={4}>
             <CCardGroup>
               <CCard className="p-4">
                 <CCardBody>
